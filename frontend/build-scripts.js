2097871b4da0dae6031612d403ae36e85c39737c
dariamorgacheva <dora.bataeva@gmail.com>
2023-06-28 11:40:29 -0400

    fix: change path in frontend/build-scripts.js to reference core api file from backend; fix npm start script

--- frontend/build-scripts.js
+++ frontend/build-scripts.js
@@ -1,45 +1,45 @@
-const execSync = require('child_process').execSync
+const execSync = require('child_process').execSync;
 
-const command = process.argv[2]
-const apiFile = process.argv[3]
-const noDeps = process.argv[4] === 'no-deps'
+const command = process.argv[2];
+const apiFile = process.argv[3];
+const noDeps = process.argv[4] === 'no-deps';
 
 const commands = {
   api: () => scripts.api(apiFile),
   build: () => {
-    !noDeps && run('npm install')
-    scripts.api(apiFile)
-    scripts.build()
+    !noDeps && run('npm install');
+    scripts.api(apiFile);
+    scripts.build();
   },
   start: () => {
-    scripts.api(apiFile)
-    scripts.start()
+    scripts.api(apiFile);
+    scripts.start();
   }
-}
+};
 
 const scripts = {
   api: (apiFile) => generateApi(apiFile),
   build: () => build(),
   start: () => start('--host 0.0.0.0 --port=3000')
-}
+};
 
 commands[command]
   ? commands[command]()
-  : error(`Command '${command}' not found, run 'npm run ${command}'`)
+  : error(`Command '${command}' not found, run 'npm run ${command}'`);
 
-function run (command) {
-  execSync(command, { stdio: 'inherit' })
+function run(command) {
+  execSync(command, { stdio: 'inherit' });
 }
 
-function build (name = '', suffix = '') {
-  run(`react-scripts build ${suffix}`)
+function build(name = '', suffix = '') {
+  run(`react-scripts build ${suffix}`);
 }
 
-function start (suffix = '') {
-  run('react-scripts start')
+function start(suffix = '') {
+  run('react-scripts start');
 }
 
-function generateApi (specFile = 'core.openapi.yaml') {
+function generateApi(specFile = '../backend/core/api/src/core.openapi.yaml') {
   execSync(
     'openapi-generator-cli generate' +
       ' -g typescript-axios ' +
@@ -48,10 +48,10 @@ function generateApi (specFile = 'core.openapi.yaml') {
       ' --server-variables=host=localhost:3000 ' +
       ' --additional-properties=stringEnums=true,enumPropertyNaming=original,removeEnumValuePrefix=false,serviceSuffix=ApiService',
     { stdio: 'inherit' }
-  )
+  );
 }
 
-function error (message) {
-  console.error(message)
-  process.exit(1)
+function error(message) {
+  console.error(message);
+  process.exit(1);
 }
