e60d8d854156cbefc21392e2836d6cefbfac9876
dariamorgacheva <dora.bataeva@gmail.com>
2023-08-02 14:14:32 -0400

    fixed submit

--- frontend/src/pages/signup/SetPasswordPage.tsx
+++ frontend/src/pages/signup/SetPasswordPage.tsx
@@ -69,87 +69,83 @@ interface SetPasswordPageProps {
   onContinueClick: () => void;
 }
 
-export const SetPasswordPage: FunctionComponent<SetPasswordPageProps> = observer((props: SetPasswordPageProps) => {
-  const [password, setPassword] = useState('');
-  const isError = props.store.isError;
-
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
-
-  return (
-    <Parent>
-      <FlexHeader>
-        <Logo>First Approval</Logo>
-        <FlexHeaderRight>
-          <Button
+export const SetPasswordPage: FunctionComponent<SetPasswordPageProps> =
+  observer((props: SetPasswordPageProps) => {
+    const isError = props.store.isError;
+    return (
+      <Parent>
+        <FlexHeader>
+          <Logo>First Approval</Logo>
+          <FlexHeaderRight>
+            <Button
             variant="outlined"
             size={'large'}
-            onClick={props.onSignInClick}>
-            Sign in
-          </Button>
-        </FlexHeaderRight>
-      </FlexHeader>
-      <FlexBodyCenter>
-        <FlexBody>
-          <Header>Welcome, {props.store.firstName}</Header>
-          <EmailLabel>Now, set your password:</EmailLabel>
-          <div>
-            <FullWidthTextField
-              value={password}
-              onChange={handlePasswordChange}
-              type={'password'}
-              label="Password 8+ characters"
-              variant="outlined"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <LockOutlined />
-                  </InputAdornment>
-                )
-              }}
-            />
-            {password && <PasswordValidation password={password} />}
-          </div>
-          {props.store.isSubmitting && <CircularProgress />}
-          {!props.store.isSubmitting && (
-            <FullWidthButton
-              variant="contained"
-              size={'large'}
-              endIcon={<ArrowForward />}
-              onClick={props.onContinueClick}>
-              Continue
-            </FullWidthButton>
+               onClick={props.onSignInClick}>
+               Sign in
+             </Button>
+           </FlexHeaderRight>
+         </FlexHeader>
+         <FlexBodyCenter>
+           <FlexBody>
+             <Header>Welcome, {props.store.firstName}</Header>
+             <EmailLabel>Now, set your password:</EmailLabel>
+             <div>
+               <FullWidthTextField
+                 value={props.store.password}
+                 onChange={(e) => {
+                   props.store.password = e.currentTarget.value;
+                  }}
+                  type={'password'}
+                  label="Password 8+ characters"
+                  variant="outlined"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <LockOutlined />
+                      </InputAdornment>
+                    )
+                  }}
+                />
+              </div>
+              {props.store.isSubmitting && <CircularProgress />}
+              {props.store.password && <PasswordValidation password={props.store.password} />}
+              {!props.store.isSubmitting && (
+                <FullWidthButton
+                  variant="contained"
+                  size={'large'}
+                  endIcon={<ArrowForward />}
+                  onClick={props.onContinueClick}>
+                  Continue
+                </FullWidthButton>
+              )}
+            </FlexBody>
+          </FlexBodyCenter>
+          {isError && (
+            <Snackbar
+              open={isError}
+              autoHideDuration={6000}
+              onClose={() => {
+                props.store.isError = false;
+              }}>
+              <Alert
+                onClose={() => {
+                  props.store.isError = false;
+                }}
+                severity="error"
+                sx={{ width: '100%' }}>
+                Registration failed
+              </Alert>
+            </Snackbar>
           )}
-        </FlexBody>
-      </FlexBodyCenter>
-      {isError && (
-        <Snackbar
-          open={isError}
-          autoHideDuration={6000}
-          onClose={() => {
-            props.store.isError = false;
-          }}>
-          <Alert
-            onClose={() => {
-              props.store.isError = false;
-            }}
-            severity="error"
-            sx={{ width: '100%' }}>
-            Registration failed
-          </Alert>
-        </Snackbar>
-      )}
-    </Parent>
-  );
-});
-
-const FullWidthTextField = styled(TextField)`
-  width: 100%;
-  margin-bottom: 20px;
-`;
-
-const EmailLabel = styled('div')`
-  margin-top: 24px;
-  margin-bottom: 24px;
-`;
+        </Parent>
+      );
+    });
+ 
+    const FullWidthTextField = styled(TextField)`
+    width: 100%;
+   margin-bottom: 20px;
+ `;
+  const EmailLabel = styled('div')`
+    margin-top: 24px;
+    margin-bottom: 24px;
+  `;
