a90e8deadc8537c63ddd57818b751ac3cc1b533c
Daria Morgacheva <74694750+dariamorgacheva@users.noreply.github.com>
2023-08-14 23:05:37 +0800

    Add password length validation (#15)
    * Add password length validation
    
    * add 3 password hints
    
    * remove test class
    
    * remove empty line
    
    * add password strength indicator
    
    * remove unnecessary changes
    
    * remove unnecessary changes
    
    * render line only after start typing password
    
    * adjust styles
    
    * adjust styles
    
    * fix colours to material-ui dark theme, fix validation range for so-so password, put validation logic to component PasswordValidation
    
    * reset package-lock.json
    
    * fixed submit
    
    ---------
    
    Co-authored-by: Vladislav Taravkov <vtaravkov@me.com>

--- frontend/src/pages/signup/SetPasswordPage.tsx
+++ frontend/src/pages/signup/SetPasswordPage.tsx
@@ -1,4 +1,5 @@
-import { type FunctionComponent, useState } from 'react';
+import React, { type FunctionComponent, useState, useEffect } from 'react';
+import { useTheme } from '@mui/material/styles';
 import {
   Alert,
   Button,
@@ -22,6 +23,45 @@ import {
   Header
 } from '../common.styled';
 import { routerStore } from '../../core/router';
+
+
+interface PasswordValidationProps {
+    password: string;
+}
+
+const PasswordValidation: FunctionComponent<PasswordValidationProps> = ({ password }) => {
+  const [passwordHint, setPasswordHint] = useState('');
+  const [passwordHintColor, setPasswordHintColor] = useState('');
+  const [lineColors, setLineColors] = useState(['', '', '']);
+  const theme = useTheme();
+
+  useEffect(() => {
+    if (password.length < 8) {
+      setPasswordHint('Please use 8+ characters for a secure password');
+      setPasswordHintColor(theme.palette.error.dark);
+      setLineColors([theme.palette.error.dark, '#D9D9D9', '#D9D9D9']);
+    } else if (password.length >= 8 && password.length < 12) {
+      setPasswordHint('So-so password');
+      setPasswordHintColor(theme.palette.warning.dark);
+      setLineColors([theme.palette.warning.dark, theme.palette.warning.dark, '#D9D9D9']);
+    } else {
+      setPasswordHint('Great password');
+      setPasswordHintColor(theme.palette.info.dark);
+      setLineColors([theme.palette.info.dark, theme.palette.info.dark, theme.palette.info.dark]);
+    }
+  }, [password, theme]);
+
+  return (
+    <>
+      <span style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'stretch', marginBottom: '16px' }}>
+        <hr style={{ width: '32%', marginLeft: '0', borderTop: `2px solid ${lineColors[0]}` }} />
+        <hr style={{ width: '32%', borderTop: `2px solid ${lineColors[1]}` }} />
+        <hr style={{ width: '32%', marginRight: '0', borderTop: `2px solid ${lineColors[2]}` }} />
+      </span>
+      <div style={{ color: passwordHintColor, fontSize: '20px', marginBottom: '16px' }}>{passwordHint}</div>
+    </>
+  );
+};
 
 interface SetPasswordPageProps {
   store: SignUpStore;
@@ -83,6 +123,7 @@ export const SetPasswordPage: FunctionComponent<SetPasswordPageProps> =
                 }}
               />
             </div>
+            {props.store.password && <PasswordValidation password={props.store.password} />}
             {props.store.isSubmitting && <CircularProgress />}
             {!props.store.isSubmitting && (
               <FullWidthButton
