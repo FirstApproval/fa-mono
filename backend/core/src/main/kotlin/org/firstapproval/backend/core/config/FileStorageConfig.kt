b1f0484834c957f2272cdfedd562d36bf60d2410
leonidsevcov <ritx222@yandex.ru>
2023-08-19 15:34:54 +0200

    encryption

--- backend/core/src/main/kotlin/org/firstapproval/backend/core/config/FileStorageConfig.kt
+++ backend/core/src/main/kotlin/org/firstapproval/backend/core/config/FileStorageConfig.kt
@@ -8,20 +8,17 @@ import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder.standard
 import org.firstapproval.backend.core.config.Properties.IpfsProperties
 import org.firstapproval.backend.core.config.Properties.S3Properties
-import org.firstapproval.backend.core.config.encryption.BYTES_ENCRYPTOR_BEAN
 import org.firstapproval.backend.core.domain.file.FileStorageService
 import org.firstapproval.backend.core.domain.ipfs.IpfsClient
-import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.crypto.encrypt.BytesEncryptor
 import org.springframework.web.client.RestTemplate
 
 @Configuration
 class FileStorageConfig {
 
     @Bean
-    fun fileStorage(s3Properties: S3Properties, applicationContext: ApplicationContext): FileStorageService {
+    fun fileStorage(s3Properties: S3Properties): FileStorageService {
         return FileStorageService(defaultS3(s3Properties)).apply {
             s3Properties.buckets.forEach {
                 createBucketIfNotExist(it)
