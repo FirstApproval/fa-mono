9aa192e6e99e3d4d4f7e10080e4457c46092e7f7
Sergei <mr.serfr@gmail.com>
2023-09-17 00:01:40 +0300

    test prod env

--- backend/core/src/main/kotlin/org/firstapproval/backend/core/FirstApprovalApplication.kt
+++ backend/core/src/main/kotlin/org/firstapproval/backend/core/FirstApprovalApplication.kt
@@ -7,17 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import java.util.TimeZone
 import java.util.TimeZone.setDefault
 
-// подключиться к эластику
-// подключиться к бд прода
-// подключиться к с3 на проде
-
-// написать лоадер для публишинга датасета
-// сделать ссылку с главной страницы на лендос прода
-// сделать оаус провайдеров для дева и прода
-
-// new publishing page
-// new research area
-
 @SpringBootApplication
 @EnableJpaRepositories("org.firstapproval.backend.core")
 @EnableElasticsearchRepositories("org.firstapproval.backend.core")
