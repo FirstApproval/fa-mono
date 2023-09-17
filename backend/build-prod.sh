962a9a02a4b431d15358fc4ad620d545c614aab0
Sergei <mr.serfr@gmail.com>
2023-09-17 20:19:19 +0300

    improved elastic searching

--- backend/build-prod.sh
+++ backend/build-prod.sh
@@ -6,5 +6,5 @@ set -e
 docker buildx build --platform linux/amd64 -f core/Dockerfile-prod -t ghcr.io/firstapproval/fa-mono-backend-prod . &&
 docker push ghcr.io/firstapproval/fa-mono-backend-prod
 
-docker buildx build -f elastic/Dockerfile -t ghcr.io/firstapproval/fa-elastic-init . &&
+docker buildx build --platform linux/amd64 -f elastic/Dockerfile -t ghcr.io/firstapproval/fa-elastic-init . &&
 docker push ghcr.io/firstapproval/fa-elastic-init
--- backend/build.sh
+++ backend/build.sh
@@ -6,5 +6,5 @@ set -e
 docker buildx build --platform linux/amd64 -f core/Dockerfile -t ghcr.io/firstapproval/fa-mono-backend . &&
 docker push ghcr.io/firstapproval/fa-mono-backend
 
-docker buildx build -f elastic/Dockerfile -t ghcr.io/firstapproval/fa-elastic-init . &&
+docker buildx build --platform linux/amd64 -f elastic/Dockerfile -t ghcr.io/firstapproval/fa-elastic-init . &&
 docker push ghcr.io/firstapproval/fa-elastic-init
