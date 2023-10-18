bcc30c47361483ea6b4dddd068c6429d76d6984e
iteleshov <egervantu@gmail.com>
2023-10-19 01:32:47 +0600

    FA-231: css for changing authors order

--- frontend/src/pages/publication/editors/styled.tsx
+++ frontend/src/pages/publication/editors/styled.tsx
@@ -1,11 +1,13 @@
 import styled from '@emotion/styled';
 
-export const LabelWrap = styled.div`
+export const LabelWrap = styled.div<{
+  marginBottom?: string;
+}>`
   font-size: 24px;
   font-style: normal;
   font-weight: 600;
 
-  margin-bottom: 32px;
+  margin-bottom: ${(props) => props.marginBottom ?? '32px'};
 `;
 
 export const SectionWrap = styled.div`
