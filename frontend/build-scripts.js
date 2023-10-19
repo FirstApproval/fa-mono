bcae008e40425b0f17184b26717f55e47eb1e731
iteleshov <egervantu@gmail.com>
2023-10-19 20:06:50 +0600

    fix-minification

--- frontend/build-scripts.js
+++ frontend/build-scripts.js
@@ -1,31 +1,31 @@
-const execSync = require('child_process').execSync;
+const execSync = require('child_process').execSync
 
-const command = process.argv[2];
-const url = process.argv[3];
-const noDeps = process.argv[4] === 'no-deps';
+const command = process.argv[2]
+const url = process.argv[3]
+const noDeps = process.argv[4] === 'no-deps'
 
 const commands = {
   api: () => scripts.api(url),
   build: () => {
-    !noDeps && run('npm install');
-    scripts.api(url);
-    scripts.build();
+    !noDeps && run('npm install')
+    scripts.api(url)
+    scripts.build()
   },
   start: () => {
-    scripts.api(url);
-    scripts.start();
+    scripts.api(url)
+    scripts.start()
   }
-};
+}
 
 const scripts = {
   api: (url) => generateApi(url),
   build: () => build(),
   start: () => start()
-};
+}
 
 commands[command]
   ? commands[command]()
-  : error(`Command '${command}' not found, run 'npm run ${command}'`);
+  : error(`Command '${command}' not found, run 'npm run ${command}'`)
 
 function run(command) {
   execSync(command, {
@@ -37,26 +37,26 @@ function run(command) {
 }
 
 function build(name = '', suffix = '') {
-  run(`react-scripts build ${suffix}`);
+  run(`react-scripts build ${suffix}`)
 }
 
 function start() {
-  run('react-scripts start');
+  run('react-scripts start')
 }
 
 function generateApi(url) {
   execSync(
     'openapi-generator-cli generate' +
-      ' -g typescript-axios ' +
-      ` -i ../backend/core/api/src/core.openapi.yaml ` +
-      ' -o src/apis/first-approval-api ' +
-      ` --server-variables=URL=${url} ` +
-      ' --additional-properties=stringEnums=true,enumPropertyNaming=original,removeEnumValuePrefix=false,serviceSuffix=ApiService',
-    { stdio: 'inherit' }
-  );
+    ' -g typescript-axios ' +
+    ` -i ../backend/core/api/src/core.openapi.yaml ` +
+    ' -o src/apis/first-approval-api ' +
+    ` --server-variables=URL=${url} ` +
+    ' --additional-properties=stringEnums=true,enumPropertyNaming=original,removeEnumValuePrefix=false,serviceSuffix=ApiService',
+    {stdio: 'inherit'}
+  )
 }
 
 function error(message) {
-  console.error(message);
-  process.exit(1);
+  console.error(message)
+  process.exit(1)
 }
