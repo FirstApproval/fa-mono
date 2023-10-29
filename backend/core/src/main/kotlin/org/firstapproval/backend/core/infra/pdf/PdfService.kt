6365a60dbfca995f29faff61c223d780b5684581
leonidsevcov <ritx222@yandex.ru>
2023-10-29 13:05:38 +0400

    PDF improvements

--- backend/core/src/main/kotlin/org/firstapproval/backend/core/infra/pdf/PdfService.kt
+++ backend/core/src/main/kotlin/org/firstapproval/backend/core/infra/pdf/PdfService.kt
@@ -1,27 +1,35 @@
 package org.firstapproval.backend.core.infra.pdf
 
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
-import com.vladmihalcea.hibernate.util.ClassLoaderUtils.getResource
+import org.apache.commons.io.IOUtils
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
 import org.thymeleaf.context.Context
 import org.thymeleaf.spring6.SpringTemplateEngine
 import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.FileOutputStream
 
 
 @Service
 class PdfService(
     private val templateEngine: SpringTemplateEngine,
+    @Value("classpath:templates/pdf/Roboto-Regular.ttf")
+    private val template: Resource
 ) {
+
+    private final val fonts: File = File.createTempFile("Roboto.ttf", "tmp")
+    init {
+        FileOutputStream(fonts).use { outputStream -> IOUtils.copy(template.inputStream, outputStream) }
+    }
 
     fun generate(templateName: String, templateContext: Context): ByteArray {
         val htmlContent = templateEngine.process(templateName, templateContext)
         ByteArrayOutputStream().use { os ->
             val builder = PdfRendererBuilder()
-            builder.useFont(
-                File(getResource("templates/pdf/Roboto-Regular.ttf").file),
-                "Roboto"
-            )
+
+            builder.useFont(fonts, "Roboto")
             builder.useFastMode()
             builder.withHtmlContent(htmlContent, null)
             builder.toStream(os)
