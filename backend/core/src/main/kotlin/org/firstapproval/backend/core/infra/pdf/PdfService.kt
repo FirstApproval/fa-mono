302d447d06dbf6f6ffc43e1e1771e7d158f10b14
leoshevt <38049287+leoshevt@users.noreply.github.com>
2023-10-29 13:07:18 +0400

    fixes for PDF, fonts, columns, etc... (#131)
    * PDF improvements
    
    * PDF improvements
    
    ---------
    
    Co-authored-by: leonidsevcov <ritx222@yandex.ru>

--- backend/core/src/main/kotlin/org/firstapproval/backend/core/infra/pdf/PdfService.kt
+++ backend/core/src/main/kotlin/org/firstapproval/backend/core/infra/pdf/PdfService.kt
@@ -1,27 +1,40 @@
 package org.firstapproval.backend.core.infra.pdf
 
-import com.lowagie.text.pdf.BaseFont
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
+import org.apache.commons.io.IOUtils
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
 import org.thymeleaf.context.Context
 import org.thymeleaf.spring6.SpringTemplateEngine
-import org.xhtmlrenderer.pdf.ITextRenderer
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+
 
 @Service
 class PdfService(
     private val templateEngine: SpringTemplateEngine,
+    @Value("classpath:templates/pdf/Roboto-Regular.ttf")
+    private val template: Resource
 ) {
 
-    fun generate(templateName: String, templateContext: Context): ByteArray {
-        val outputStream = ByteArrayOutputStream()
-        val html = templateEngine.process(templateName, templateContext)
+    private final val fonts: File = File.createTempFile("Roboto.ttf", "tmp")
+    init {
+        FileOutputStream(fonts).use { outputStream -> IOUtils.copy(template.inputStream, outputStream) }
+    }
 
-        val renderer = ITextRenderer()
-        renderer.fontResolver.addFont("templates/pdf/Roboto-Regular.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED)
-        renderer.setDocumentFromString(html)
-        renderer.layout()
-        renderer.createPDF(outputStream)
-        outputStream.close()
-        return outputStream.toByteArray()
+    fun generate(templateName: String, templateContext: Context): ByteArray {
+        val htmlContent = templateEngine.process(templateName, templateContext)
+        ByteArrayOutputStream().use { os ->
+            val builder = PdfRendererBuilder()
+
+            builder.useFont(fonts, "Roboto")
+            builder.useFastMode()
+            builder.withHtmlContent(htmlContent, null)
+            builder.toStream(os)
+            builder.run()
+            return os.toByteArray()
+        }
     }
 }
