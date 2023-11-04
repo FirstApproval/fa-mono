e0716538f7ee7d9697e7ce4671b5e7735a4a6a98
leonidsevcov <ritx222@yandex.ru>
2023-11-05 01:59:44 +0400

    sha256 frontend calculation

--- frontend/src/util/sha256Util.ts
+++ frontend/src/util/sha256Util.ts
@@ -2,7 +2,7 @@ import { sha256 } from 'js-sha256';
 
 export async function calculateSHA256(file: File): Promise<string> {
   return new Promise((resolve) => {
-    const chunkSize = 10 * 1024 * 1024; // Размер куска (5 МБ в данном случае)
+    const chunkSize = 10 * 1024 * 1024; // Размер куска (10 МБ в данном случае)
     const hash = sha256.create();
 
     const calculateChunk = (start: number): void => {
