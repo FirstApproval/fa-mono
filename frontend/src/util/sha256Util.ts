98928e6e906763845279a7308780d23681f6304d
leonidsevcov <ritx222@yandex.ru>
2023-11-05 01:56:16 +0400

    sha256 frontend calculation

--- frontend/src/util/sha256Util.ts
+++ frontend/src/util/sha256Util.ts
@@ -1,17 +1,27 @@
+import { sha256 } from 'js-sha256';
+
 export async function calculateSHA256(file: File): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const fileReader = new FileReader();
-    fileReader.onload = function () {
-      const arrayBuffer: any = this.result;
-      crypto.subtle
-        .digest('SHA-256', arrayBuffer)
-        .then((hashBuffer) => {
-          const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return new Promise((resolve) => {
+    const chunkSize = 10 * 1024 * 1024; // Размер куска (5 МБ в данном случае)
+    const hash = sha256.create();
+
+    const calculateChunk = (start: number): void => {
+      const reader = new FileReader();
+      const blob = file.slice(start, start + chunkSize);
+
+      reader.onload = function (event) {
+        const data = event.target!.result as ArrayBuffer;
+        hash.update(new Uint8Array(data));
+
+        if (start + chunkSize < file.size) {
+          calculateChunk(start + chunkSize);
+        } else {
+          const hashArray = Array.from(new Uint8Array(hash.arrayBuffer()));
           resolve(btoa(String.fromCharCode.apply(null, hashArray)));
-        })
-        .catch(reject);
+        }
+      };
+      reader.readAsArrayBuffer(blob);
     };
-    fileReader.onerror = reject;
-    fileReader.readAsArrayBuffer(file);
+    calculateChunk(0);
   });
 }
