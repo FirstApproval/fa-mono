9f0120d28c3979924bab2e5a52ff933a5ba27966
leonidsevcov <ritx222@yandex.ru>
2023-11-05 10:53:59 +0400

    sha256 frontend calculation

--- frontend/src/util/sha256Util.ts
+++ frontend/src/util/sha256Util.ts
@@ -1,6 +1,14 @@
 import { sha256 } from 'js-sha256';
 
 export async function calculateSHA256(file: File): Promise<string> {
+  if (file.size > 500 * 1024 * 1024) {
+    return sha256ForFileByParts(file);
+  } else {
+    return sha256ForFile(file);
+  }
+}
+
+export async function sha256ForFileByParts(file: File): Promise<string> {
   return new Promise((resolve) => {
     const chunkSize = 10 * 1024 * 1024; // Размер куска (10 МБ в данном случае)
     const hash = sha256.create();
@@ -14,7 +22,9 @@ export async function calculateSHA256(file: File): Promise<string> {
         hash.update(new Uint8Array(data));
 
         if (start + chunkSize < file.size) {
-          calculateChunk(start + chunkSize);
+          setTimeout(() => {
+            calculateChunk(start + chunkSize);
+          }, 10);
         } else {
           const hashArray = Array.from(new Uint8Array(hash.arrayBuffer()));
           resolve(btoa(String.fromCharCode(...hashArray)));
@@ -23,5 +33,23 @@ export async function calculateSHA256(file: File): Promise<string> {
       reader.readAsArrayBuffer(blob);
     };
     calculateChunk(0);
+  });
+}
+
+export async function sha256ForFile(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.onload = function () {
+      const arrayBuffer: any = this.result;
+      crypto.subtle
+        .digest('SHA-256', arrayBuffer)
+        .then((hashBuffer) => {
+          const hashArray = Array.from(new Uint8Array(hashBuffer));
+          resolve(btoa(String.fromCharCode.apply(null, hashArray)));
+        })
+        .catch(reject);
+    };
+    fileReader.onerror = reject;
+    fileReader.readAsArrayBuffer(file);
   });
 }
