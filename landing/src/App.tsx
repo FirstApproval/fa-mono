2dc527f80269e4fb05df1f353d09f000dd294988
Vladislav Taravkov <vtaravkov@me.com>
2023-11-08 17:00:56 +0500

    fa landing

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,3 +1,4 @@
+import './App.css';
 import Grid from '@mui/material/Grid/Grid';
 import { ReactElement } from 'react';
 import { Main } from './landing/Main';
@@ -6,18 +7,35 @@ import { Hinder } from './landing/Hinder';
 import { Terms } from './landing/Terms';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './theme';
+import styled from '@emotion/styled';
+import { DataFirst } from './landing/DataFirst';
 
 const LandingApp = (): ReactElement => {
   return (
     <ThemeProvider theme={theme}>
-      <Grid container spacing={2}>
-        <Main />
-        <DatasetUpload />
-        <Terms />
-        <Hinder />
-      </Grid>
+      <FlexWrap>
+        <PageWrap>
+          <Grid container spacing={2}>
+            <Main />
+            <DatasetUpload />
+            <Terms />
+            <Hinder />
+            <DataFirst />
+          </Grid>
+        </PageWrap>
+      </FlexWrap>
     </ThemeProvider>
   );
 };
 
 export default LandingApp;
+
+const PageWrap = styled.div`
+  max-width: 1980px;
+`;
+
+const FlexWrap = styled.div`
+  //display: flex;
+  //flex-direction: column;
+  //align-items: center;
+`;
