f45ccb95a5e4a0892874680e171d7f05798beca3
Sergei <mr.serfr@gmail.com>
2024-01-01 15:59:25 +0800

    team page

--- team/build-prod.sh
+++ team/build-prod.sh
@@ -3,5 +3,5 @@
 set -e
 
 npm run build &&
-docker buildx build --platform linux/amd64 -f Dockerfile -t ghcr.io/firstapproval/fa-mono-landing-prod . &&
-docker push ghcr.io/firstapproval/fa-mono-landing-prod
+docker buildx build --platform linux/amd64 -f Dockerfile -t ghcr.io/firstapproval/fa-mono-team-prod . &&
+docker push ghcr.io/firstapproval/fa-mono-team-prod
