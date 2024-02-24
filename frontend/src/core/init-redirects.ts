e9dc3515aa5439a8662a8b51dc0738a99f7db741
iteleshov <egervantu@gmail.com>
2024-02-25 00:31:46 +0700

    always redirect unauthorized users to intro page

--- frontend/src/core/init-redirects.ts
+++ frontend/src/core/init-redirects.ts
@@ -5,19 +5,12 @@ const history = createBrowserHistory();
 
 export async function initRedirects(): Promise<void> {
   const _path: string = history.location.pathname;
-  const introViewed = localStorage.getItem('intro_viewed') === 'true';
-  if (!introViewed) {
-    localStorage.setItem('intro_viewed', 'true');
-    window.location.replace('https://intro.dev.firstapproval.io/');
-    return;
-  }
-
   const isAuthorizedUser = !!localStorage.getItem('access-token');
   const isSignUpPath = window.location.href.endsWith(signUpPath);
   const isSignInPath = window.location.href.endsWith(signInPath);
   const isOauthCallback = !!pathToOauthType[_path];
 
   if (!isAuthorizedUser && !isSignUpPath && !isSignInPath && !isOauthCallback) {
-    window.location.replace(signUpPath);
+    window.location.replace('https://intro.dev.firstapproval.io/');
   }
 }
