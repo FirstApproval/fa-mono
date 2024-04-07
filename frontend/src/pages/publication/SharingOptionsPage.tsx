b0e7374a5adfb813c490b7555f9d384664a1deb6
iteleshov <egervantu@gmail.com>
2024-04-07 18:51:20 +0500

    FA-276: co-authorship

--- frontend/src/pages/publication/SharingOptionsPage.tsx
+++ frontend/src/pages/publication/SharingOptionsPage.tsx
@@ -12,12 +12,14 @@ import {
   Typography
 } from '@mui/material';
 import {
-  ViewInArOutlined,
+  AlternateEmail,
   Close,
   CloudOutlined,
   MessageOutlined,
-  Public
+  Public,
+  ViewInArOutlined
 } from '@mui/icons-material';
+import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 import Button from '@mui/material/Button';
 import { routerStore } from '../../core/router';
 import { publicationService } from '../../core/service';
@@ -156,6 +158,29 @@ export const SharingOptionsPage = (props: {
               Your data annotation text is always accessible to everyone.
             </OnlyTheFilesYouUploadWrap>
             <HeightElement value={'24px'} />
+            <SharingOptionsWrap>
+              <SharingOption
+                icon={<FormatQuoteIcon fontSize={'medium'} />}
+                label={'Citation is enough'}
+                description={
+                  'Others may use your data, provided that they cite your dataset in their research.'
+                }
+                height={'128px'}
+                isSelected
+              />
+              <Tooltip
+                title="Be credited as a co-author in journal publications when your data is vital to others'
+              research. You can accept or reject collaboration requests.">
+                <SharingOption
+                  icon={<AlternateEmail fontSize={'medium'} />}
+                  label={'Co-authorship requirement'}
+                  description={''}
+                  isDisabled={true}
+                  noMargin={true}
+                  height={'128px'}
+                />
+              </Tooltip>
+            </SharingOptionsWrap>
             <Typography variant={'h6'}>Your files storage</Typography>
             <SharingOptionsWrap>
               <SharingOption
@@ -175,6 +200,7 @@ export const SharingOptionsPage = (props: {
                   'Distribute dataset across a decentralized network for added resilience and permanence. Maximum dataset size 2GB '
                 }
                 noMargin={true}
+                isDisabled={isIpfsDisabled}
               />
               {/* <FormControlLabel */}
               {/*  value={StorageType.CLOUD_SECURE_STORAGE} */}
@@ -324,15 +350,17 @@ const SharingOption = React.forwardRef<HTMLDivElement, SharingOptionsProps>(
         isSelected={isSelected}
         height={height}
         noMargin={noMargin}>
-        <FlexWrapRow>
-          <IconWrap isDisabled={isDisabled}>{props.icon}</IconWrap>
-          <SharingOptionLabel variant={'h6'} isDisabled={isDisabled}>
-            {label}
-          </SharingOptionLabel>
-        </FlexWrapRow>
-        <SharingOptionDescription isDisabled={isDisabled}>
-          <Typography variant={'body2'}>{description}</Typography>
-        </SharingOptionDescription>
+        <div>
+          <FlexWrapRow>
+            <IconWrap isDisabled={isDisabled}>{props.icon}</IconWrap>
+            <SharingOptionLabel isDisabled={isDisabled}>
+              {label}
+            </SharingOptionLabel>
+          </FlexWrapRow>
+          <SharingOptionDescription isDisabled={isDisabled}>
+            <Typography variant={'body2'}>{description}</Typography>
+          </SharingOptionDescription>
+        </div>
         {isDisabled && (
           <FlexWrapRow>
             <SoonChip />
@@ -425,10 +453,10 @@ const SharingOptionWrap = styled.div<{
   margin-right: ${(props) => (props.noMargin ? '0px' : '24px')};
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
 `;
 
 const IconWrap = styled.div<{ isDisabled?: boolean }>`
-  margin-top: 4px;
   margin-right: 8px;
   ${(props) =>
     props.isDisabled
@@ -437,6 +465,12 @@ const IconWrap = styled.div<{ isDisabled?: boolean }>`
 `;
 
 const SharingOptionLabel = styled(Typography)<{ isDisabled?: boolean }>`
+  font-family: Roboto;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%; /* 24px */
+  letter-spacing: 0.15px;
   ${(props) =>
     props.isDisabled
       ? 'color: var(--text-disabled, rgba(4, 0, 54, 0.38));'
@@ -444,8 +478,7 @@ const SharingOptionLabel = styled(Typography)<{ isDisabled?: boolean }>`
 `;
 
 const SharingOptionDescription = styled.div<{ isDisabled?: boolean }>`
-  margin-top: auto;
-  color: gray;
+  color: var(--text-secondary, #68676e);
 `;
 
 const HeaderWrap = styled.div`
@@ -510,12 +543,6 @@ const SharingOptionsWrap = styled.div`
   justify-content: space-between;
   padding-top: 24px;
   padding-bottom: 24px;
-`;
-
-const StorageOptionsWrap = styled.div`
-  margin-top: 48px;
-  display: flex;
-  flex-direction: column;
 `;
 
 const StorageOptionLabelWrap = styled.div<{
