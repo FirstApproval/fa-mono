1dd037d13ac0431bb4fede4590be7a047ff3ca99
iteleshov <egervantu@gmail.com>
2024-04-11 01:31:30 +0500

    FA-276: added agreeToTheFirstApprovalLicense to DownloadHistory

--- backend/core/src/main/resources/db/migration/V72__add_last_download_time_to_downloaders.sql
+++ backend/core/src/main/resources/db/migration/V72__add_last_download_time_to_downloaders.sql
@@ -4,8 +4,7 @@ ALTER TABLE downloaders
 UPDATE downloaders
 SET last_download_time = (SELECT MAX(history_item ->> 'creationTime')::timestamp
                           FROM (SELECT jsonb_array_elements(history) AS history_item
-                                FROM downloaders
-                                WHERE id = :downloaderId) AS history_items);
+                                FROM downloaders) AS history_items);
 
 ALTER TABLE downloaders
     ALTER COLUMN last_download_time SET NOT NULL;
