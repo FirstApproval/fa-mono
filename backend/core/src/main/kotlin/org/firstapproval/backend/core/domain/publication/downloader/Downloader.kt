3e6284a8b7d701842d5f708a5a2f10768cd44d6d
iteleshov <egervantu@gmail.com>
2024-04-11 01:52:54 +0500

    FA-276: added agreeToTheFirstApprovalLicense to DownloadHistory

--- backend/core/src/main/kotlin/org/firstapproval/backend/core/domain/publication/downloader/Downloader.kt
+++ backend/core/src/main/kotlin/org/firstapproval/backend/core/domain/publication/downloader/Downloader.kt
@@ -1,6 +1,10 @@
 package org.firstapproval.backend.core.domain.publication.downloader
 
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType
+import jakarta.persistence.AttributeConverter
+import jakarta.persistence.Column
+import jakarta.persistence.Convert
+import jakarta.persistence.Converter
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.OneToOne
@@ -8,13 +12,15 @@ import jakarta.persistence.Table
 import org.firstapproval.backend.core.domain.publication.Publication
 import org.firstapproval.backend.core.domain.user.User
 import org.hibernate.annotations.Type
+import java.sql.Timestamp
+import java.time.ZoneId.systemDefault
 import java.time.ZonedDateTime
 import java.util.UUID
 import java.util.UUID.randomUUID
 
 @Entity
 @Table(name = "downloaders")
-class Downloader (
+class Downloader(
     @Id
     var id: UUID = randomUUID(),
     @OneToOne
@@ -28,7 +34,21 @@ class Downloader (
     var lastDownloadTime: ZonedDateTime = ZonedDateTime.now()
 )
 
-class DownloadHistory (
+class DownloadHistory(
     var agreeToTheFirstApprovalLicense: Boolean,
+    @Convert(converter = ZonedDateTimeConverter::class)
+    @Column(columnDefinition = "timestamp with time zone")
     var creationTime: ZonedDateTime
 )
+
+@Converter(autoApply = true)
+class ZonedDateTimeConverter : AttributeConverter<ZonedDateTime, Timestamp> {
+
+    override fun convertToDatabaseColumn(attribute: ZonedDateTime?): Timestamp? {
+        return attribute?.let { Timestamp.from(it.toInstant()) }
+    }
+
+    override fun convertToEntityAttribute(dbData: Timestamp?): ZonedDateTime? {
+        return dbData?.toInstant()?.atZone(systemDefault())
+    }
+}
