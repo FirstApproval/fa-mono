fd1e78ddfd7ee56fc8b2527d5cecafdafe84e8c5
iteleshov <egervantu@gmail.com>
2024-12-24 20:58:02 +0530

    FA-253: ci - github actions

--- backend/build-prod.sh
+++ backend/build-prod.sh
@@ -3,7 +3,7 @@
 set -e
 
 ./gradlew clean bootJar &&
-docker buildx build --platform linux/amd64 -f core/Dockerfile-prod -t ghcr.io/firstapproval/fa-mono-backend-prod . &&
+docker buildx build --platform linux/amd64 -f core/Dockerfile-prod-local -t ghcr.io/firstapproval/fa-mono-backend-prod . &&
 docker push ghcr.io/firstapproval/fa-mono-backend-prod
 
 docker buildx build --platform linux/amd64 -f elastic/Dockerfile -t ghcr.io/firstapproval/fa-elastic-init . &&
--- frontend/build-prod.sh
+++ frontend/build-prod.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+cp ../backend/core/api/src/core.openapi.yaml ../frontend/core.openapi.yaml &&
 npm run build-prod &&
 docker buildx build --platform linux/amd64 -f Dockerfile -t ghcr.io/firstapproval/fa-mono-frontend-prod . &&
 docker push ghcr.io/firstapproval/fa-mono-frontend-prod
