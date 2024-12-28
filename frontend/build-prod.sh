4a64974c8e285dacc9fd5023f874d9aa970b66f4
iteleshov <egervantu@gmail.com>
2024-12-29 01:34:59 +0530

    FA-277: fix build-prod.sh

--- frontend/build-prod.sh
+++ frontend/build-prod.sh
@@ -4,5 +4,5 @@ set -e
 
 cp ../backend/core/api/src/core.openapi.yaml ../frontend/core.openapi.yaml &&
 npm run build-prod &&
-docker buildx build --platform linux/amd64 -f Dockerfile -t ghcr.io/firstapproval/fa-mono-frontend-prod . &&
+docker buildx build --platform linux/amd64 -f Dockerfile-local -t ghcr.io/firstapproval/fa-mono-frontend-prod . &&
 docker push ghcr.io/firstapproval/fa-mono-frontend-prod
