99831d73e0dd23ead8877a4fb354c0a4804b4b93
iteleshov <egervantu@gmail.com>
2025-01-05 16:49:25 +0530

    not redirect to intro from Contest page.

--- frontend/src/core/init-redirects.ts
+++ frontend/src/core/init-redirects.ts
@@ -1,6 +1,8 @@
+import { contestPath } from './router/constants';
 export async function initRedirects(): Promise<void> {
   const introViewed = localStorage.getItem('intro_viewed') === 'true';
-  if (!introViewed) {
+  const isContestPage = window.location.pathname.startsWith(contestPath)
+  if (!introViewed && !isContestPage) {
     localStorage.setItem('intro_viewed', 'true');
     window.location.replace('https://intro.dev.firstapproval.io/');
   }
