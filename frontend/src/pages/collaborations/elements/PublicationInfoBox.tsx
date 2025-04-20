16c07771f252044d17ee9fdf7b8e611f7bdf7501
iteleshov <egervantu@gmail.com>
2025-04-20 17:11:34 +0500

    create PublicationCollaborations.tsx

--- frontend/src/pages/collaborations/elements/PublicationInfoBox.tsx
+++ frontend/src/pages/collaborations/elements/PublicationInfoBox.tsx
@@ -26,21 +26,6 @@ export const PublicationInfoBox = observer(
 
     debugger;
 
-    const [utilAnchor, setUtilAnchor] = useState<null | HTMLElement>(null);
-    const openUtilMenu = Boolean(utilAnchor);
-
-    const [reportProblemOpened, setReportProblemOpened] = useState(false);
-
-    const handleUtilMenuClick = (
-      event: React.MouseEvent<HTMLButtonElement>
-    ): void => {
-      setUtilAnchor(event.currentTarget);
-    };
-
-    const handleUtilMenuClose = (): void => {
-      setUtilAnchor(null);
-    };
-
     return (
       <FlexWrapRowSpaceBetween variant={'body2'} component={'div'}>
         <FlexWrapRow>
@@ -107,45 +92,8 @@ export const FlexWrapRowSpaceBetween = styled(Typography)`
   color: #68676e;
 ` as typeof Typography;
 
-const LicensingLinkWrap = styled.div`
-  cursor: pointer;
-
-  color: var(--text-disabled, rgba(4, 0, 54, 0.38));
-
-  /* typography/caption */
-  font-family: Roboto;
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 166%; /* 19.92px */
-  letter-spacing: 0.4px;
-`;
-
 export const FlexWrapRow = styled.div`
   display: flex;
   align-items: center;
   height: 100%;
-`;
-
-const UnderCollaborationRequirements = styled(Typography)`
-  display: flex;
-  height: 48px;
-  padding: 8px 22px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  color: var(--text-disabled, rgba(4, 0, 54, 0.38));
-`;
-
-const RequestCollaborationButton = styled(Button)`
-  display: flex;
-  height: 32px;
-  padding: 4px 10px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  border: 1px solid;
-
-  margin-left: 24px;
 `;
