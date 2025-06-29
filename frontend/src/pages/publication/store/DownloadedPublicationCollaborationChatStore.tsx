492bdce6549029728b265f0dd8654ac4df830217
iteleshov <egervantu@gmail.com>
2025-06-30 02:16:45 +0500

    add e CollaborationMessageType

--- frontend/src/pages/publication/store/DownloadedPublicationCollaborationChatStore.tsx
+++ frontend/src/pages/publication/store/DownloadedPublicationCollaborationChatStore.tsx
@@ -3,16 +3,22 @@ import {
   CollaborationMessageType,
   CollaborationRequestMessage,
   Publication,
-  UserInfo
-} from '../../../apis/first-approval-api';
+  UserInfo,
+  UseType
+} from "../../../apis/first-approval-api"
 import { userStore } from '../../../core/user';
-import { collaborationRequestService, publicationService } from "../../../core/service"
-import { CollaborationChatInterface } from "./CollaborationChatStore"
+import {
+  collaborationRequestChatService,
+  publicationService
+} from '../../../core/service';
+import { CollaborationChatInterface } from './CollaborationChatStore';
 
-export class DownloadedPublicationCollaborationChatStore implements CollaborationChatInterface {
+export class DownloadedPublicationCollaborationChatStore
+  implements CollaborationChatInterface
+{
   collaborationRequestId: string = '';
   collaborationRequestCreator: UserInfo | undefined = undefined;
-  messages: CollaborationRequestMessage[] = [];
+  messages: CollaborationRequestMessage[] | undefined = undefined;
   publication?: Publication;
   publicationCreator: UserInfo | undefined = undefined;
 
@@ -20,21 +26,40 @@ export class DownloadedPublicationCollaborationChatStore implements Collaboratio
     makeAutoObservable(this);
 
     this.collaborationRequestCreator = userStore.user;
-    publicationService.getPublication(publicationId).then(response => {
+    publicationService.getPublication(publicationId).then(async (response) => {
       this.publication = response.data;
       this.publicationCreator = response.data.creator;
 
-      // if (collaborationRequestService.getCollaborationRequest())
-      this.messages = [
-        {
-          type: CollaborationMessageType.CREATE_REQUEST,
-          isAssistant: true,
-          text:
-            `The dataset "${this.publication?.title}" was downloaded.\n\n` +
-            'This dataset was published in open access by the author(s).\n' +
-            'If you reuse it in your work, it is enough for you to cite this dataset.'
+      switch (this.publication.useType) {
+        case (UseType.CO_AUTHORSHIP): {
+          await this.loadInitialState(publicationId)
+          break;
         }
-      ]
+        case (UseType.CITATION): {
+          this.messages = [
+            {
+              type: CollaborationMessageType.CREATE_REQUEST,
+              isAssistant: true,
+              text:
+                `The dataset "${this.publication?.title}" was downloaded.\n\n` +
+                'This dataset was published in open access by the author(s).\n' +
+                'If you reuse it in your work, it is enough for you to cite this dataset.'
+            }
+          ]
+          break;
+        }
+      }
+      // if (collaborationRequestService.getCollaborationRequest())
     });
+  }
+
+  private async loadInitialState(publicationId: string): Promise<void> {
+    const response = await collaborationRequestChatService.getCollaborationChatByPublicationId(publicationId);
+    const data = response.data;
+    this.collaborationRequestId = data.collaborationRequestId;
+    this.publicationCreator = data.publicationCreator;
+    this.collaborationRequestCreator = data.collaborationRequestCreator;
+    this.messages = data.messages;
+    debugger;
   }
 }
