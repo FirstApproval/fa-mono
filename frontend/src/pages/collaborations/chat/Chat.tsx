0229c2b4edbb97b92062838eba09586e8e74f027
iteleshov <egervantu@gmail.com>
2025-07-09 15:26:36 +0500

    show PersonalData element only if it is last message

--- frontend/src/pages/collaborations/chat/Chat.tsx
+++ frontend/src/pages/collaborations/chat/Chat.tsx
@@ -227,17 +227,23 @@ const Chat: React.FC<ChatProps> = observer((props: { collaborationChatStore: Dow
                 {message.text}
 
                 {message.type === CollaborationMessageType.IF_YOU_ARE_INTERESTED_IN_THIS_DATASET && showStepsInfo()}
-                {message.type === CollaborationMessageType.VERIFY_YOUR_NAME_AND_AFFILIATION &&
-                  <PersonalData
-                    handlePublicationFormMsg={handlePublicationFormMsg}
-                    store={collaborationChatStore}
-                  />
-                }
+                {/* {message.type === CollaborationMessageType.VERIFY_YOUR_NAME_AND_AFFILIATION && */}
+                {/*   <PersonalData */}
+                {/*     handlePublicationFormMsg={handlePublicationFormMsg} */}
+                {/*     store={collaborationChatStore} */}
+                {/*   /> */}
+                {/* } */}
               </Message>
               <HeightElement value={'32px'} />
             </React.Fragment>
           );
         })}
+        {collaborationChatStore.messageType === CollaborationMessageType.VERIFY_YOUR_NAME_AND_AFFILIATION &&
+          <PersonalData
+            handlePublicationFormMsg={handlePublicationFormMsg}
+            store={collaborationChatStore}
+          />
+        }
         {/* {collaborationChatStore.messageType === CollaborationMessageType.CITATION_IS_ENOUGH && ( */}
         {/*   <UserActions */}
         {/*     messageType={CollaborationMessageType.CITATION_IS_ENOUGH} */}
