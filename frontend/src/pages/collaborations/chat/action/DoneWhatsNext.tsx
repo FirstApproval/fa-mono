54d33c6ced38b017715302f80c15373a12a16077
iteleshov <egervantu@gmail.com>
2025-07-17 15:57:51 +0500

    improved DoneWhatsNext.tsx

--- frontend/src/pages/collaborations/chat/action/DoneWhatsNext.tsx
+++ frontend/src/pages/collaborations/chat/action/DoneWhatsNext.tsx
@@ -1,6 +1,8 @@
 import { CollaborationMessageType, CollaborationPotentialPublicationData } from "../../../../apis/first-approval-api"
 import { UserAction } from "./UserAction"
 import { DownloadedPublicationCollaborationChatStore } from "../../../publication/store/DownloadedPublicationCollaborationChatStore"
+import { getFullName } from "../../../../util/userUtil"
+import React from "react"
 
 function doneWhatsNextAction(collaborationChatStore: DownloadedPublicationCollaborationChatStore): void {
   const payload: CollaborationPotentialPublicationData = {
@@ -18,12 +20,23 @@ function doneWhatsNextAction(collaborationChatStore: DownloadedPublicationCollab
     payload,
     text: "Done. What’s next?"
   }).then(response => {
+    const creator = collaborationChatStore.publication!!.creator!!;
+    const otherAuthors = collaborationChatStore.publication!!.authors!!
+      .filter(author => author.user!!.id !== creator.id)
+
+    const mappedAuthors = otherAuthors.map(
+      author => {`• ${author!!.firstName} ${author!!.lastName} FA Collaboration Agreement.pdf`}
+    ).join('\n');
     collaborationChatStore.sendMessage({
       type: CollaborationMessageType.PREFILLED_COLLABORATION_AGREEMENT,
       isAssistant: true,
-      text: "Good job! Here is a pre-filled (unsigned) collaboration agreement with the corresponding author: \n" +
+      text: (
+        "Good job! Here is a pre-filled (unsigned) collaboration agreement with the corresponding author: \n" +
+        `${getFullName(creator)} - FA Collaboration Agreement.pdf` +
         "And the rest of agreements (they differ only in information about the data authors): \n" +
+        `${mappedAuthors}\n` +
         "Please review the agreement(s), and if all information is correct, sign and send it them by clicking the button below."
+      )
     }, CollaborationMessageType.PREFILLED_COLLABORATION_AGREEMENT).then();
   }).then();
 }
