53af2da304400d7b298def82e194d3cad181a60c
iteleshov <egervantu@gmail.com>
2025-07-23 01:23:39 +0500

    refactoring DoneWhatsNext.tsx. used sendMessages for sending both messages in one request

--- frontend/src/pages/collaborations/chat/action/DoneWhatsNext.tsx
+++ frontend/src/pages/collaborations/chat/action/DoneWhatsNext.tsx
@@ -1,31 +1,47 @@
-import { CollaborationMessageType, CollaborationPotentialPublicationData } from "../../../../apis/first-approval-api"
+import {
+  CollaborationMessageType,
+  CollaborationRequestMessage
+} from "../../../../apis/first-approval-api"
 import { UserAction } from "./UserAction"
 import { DownloadedPublicationCollaborationChatStore } from "../../../publication/store/DownloadedPublicationCollaborationChatStore"
 import { getFullName } from "../../../../util/userUtil"
 import React from "react"
 
 function doneWhatsNextAction(collaborationChatStore: DownloadedPublicationCollaborationChatStore): void {
-  const payload: CollaborationPotentialPublicationData = {
-    potentialPublicationTitle: collaborationChatStore.potentialPublicationName!!,
-    typeOfWork: collaborationChatStore.typeOfWork!!,
-    intendedJournalForPublication: collaborationChatStore.intendedJournalForPublication,
-    detailsOfResearch: collaborationChatStore.detailsOfResearch,
-    type: CollaborationMessageType.DONE_WHATS_NEXT,
-  }
-  collaborationChatStore.sendMessage({
+  const doneWhatsNextMessage = {
     type: CollaborationMessageType.DONE_WHATS_NEXT,
     isAssistant: false,
-    payload,
+    payload: {
+      potentialPublicationTitle: collaborationChatStore.potentialPublicationName!!,
+      typeOfWork: collaborationChatStore.typeOfWork!!,
+      intendedJournalForPublication: collaborationChatStore.intendedJournalForPublication,
+      detailsOfResearch: collaborationChatStore.detailsOfResearch,
+      type: CollaborationMessageType.DONE_WHATS_NEXT,
+    },
     text: "Done. What’s next?"
-  }).then(response => {
+  }
+
+  const prefilledCollaborationAgreementMessage = createPrefilledAgreementMessage(collaborationChatStore);
+  collaborationChatStore.sendMessages([doneWhatsNextMessage, prefilledCollaborationAgreementMessage]).then();
+}
+
+export const doneWhatsNext: UserAction = {
+  text: 'Done. What’s next?',
+  action: (collaborationChatStore: DownloadedPublicationCollaborationChatStore) => doneWhatsNextAction(collaborationChatStore)
+}
+
+function createPrefilledAgreementMessage(
+  collaborationChatStore: DownloadedPublicationCollaborationChatStore
+): CollaborationRequestMessage {
     const creator = collaborationChatStore.publication!!.creator!!;
     const otherAuthors = collaborationChatStore.publication!!.authors!!
-      .filter(author => author.user!!.id !== creator.id)
+      .filter(author => author.user && author.user!!.id !== creator.id)
 
     const mappedAuthors = otherAuthors.map(
       author => {`• ${author!!.firstName} ${author!!.lastName} FA Collaboration Agreement.pdf`}
     ).join('\n');
-    collaborationChatStore.sendMessage({
+
+    return {
       type: CollaborationMessageType.PREFILLED_COLLABORATION_AGREEMENT,
       isAssistant: true,
       text: (
@@ -35,11 +51,5 @@ function doneWhatsNextAction(collaborationChatStore: DownloadedPublicationCollab
         `${mappedAuthors}\n` +
         "Please review the agreement(s), and if all information is correct, sign and send it them by clicking the button below."
       )
-    }, CollaborationMessageType.PREFILLED_COLLABORATION_AGREEMENT).then();
-  }).then();
-}
-
-export const doneWhatsNext: UserAction = {
-  text: 'Done. What’s next?',
-  action: (collaborationChatStore: DownloadedPublicationCollaborationChatStore) => doneWhatsNextAction(collaborationChatStore)
+    };
 }
