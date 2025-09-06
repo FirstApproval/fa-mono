8c4cdf68fabf21922f11d580b496bd97ae5e8814
iteleshov <egervantu@gmail.com>
2025-09-06 13:22:15 +0500

    Revert "Revert "FA-282: update to aws s3 v2 (#176)""
    This reverts commit a574b68176fe280e634a554c912e7abfc3f2964e.

--- backend/core/src/main/kotlin/org/firstapproval/backend/core/domain/publication/file/PublicationFileService.kt
+++ backend/core/src/main/kotlin/org/firstapproval/backend/core/domain/publication/file/PublicationFileService.kt
@@ -1,6 +1,5 @@
 package org.firstapproval.backend.core.domain.publication.file
 
-import com.amazonaws.services.s3.model.S3Object
 import org.apache.commons.io.FilenameUtils
 import org.firstapproval.backend.core.domain.publication.PublicationRepository
 import org.firstapproval.backend.core.domain.publication.checkAccessToPublication
@@ -103,16 +102,6 @@ class PublicationFileService(
             publicationFileRepository.delete(it)
             it.id
         }
-    }
-
-    @Transactional(readOnly = true)
-    fun getPublicationFileWithContent(user: User, fileId: UUID): FileResponse {
-        val file = publicationFileRepository.getReferenceById(fileId)
-        checkAccessToPublication(user, file.publication)
-        return FileResponse(
-            name = file.name,
-            s3Object = fileStorageService.get(FILES, fileId.toString())
-        )
     }
 
     fun deleteFiles(user: User, fileIds: List<UUID>) {
@@ -227,8 +216,3 @@ class PublicationFileService(
         }
     }
 }
-
-data class FileResponse(
-    val name: String,
-    val s3Object: S3Object
-)
